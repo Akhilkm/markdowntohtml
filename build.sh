d068a313bb72a3055f84be31f7f32fce8790278d
Akhil KM <akhilu011@gmail.com>
2020-01-28 12:21:49 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pandoc sparkInstallation.md -f markdown -t html -s -o test1.html
+#pandoc sparkInstallation.md -f markdown -t html -s -o test1.html
+
+for file in *.md; 
+do 
+    fileName="$(cut -d'.' -f1 <<<$file)"
+    echo "pandoc $file -f markdown -t html --metadata title=$fileName -s -o $fileName.html"
+    pandoc $file -f markdown -t html --metadata title=$fileName -s -o $fileName.html
+    echo "Processing $f file.."; 
+done
